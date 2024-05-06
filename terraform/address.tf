bf406be942478a4a1b3fa8ec92a61510e766e3c3
David Driver <davidallendriver@gmail.com>
2024-05-06 00:08:02 +0000

    use regional IP instead of global

--- terraform/address.tf
+++ terraform/address.tf
@@ -1,6 +1,7 @@
 # Global compute addresses are used for HTTP(S) load balancing
 # We will assign one here to be used by our cluster's nginx
-resource "google_compute_global_address" "default" {
+resource "google_compute_address" "default" {
   name = var.lb_ip_name
-  address = var.lb_ip_addr
+  region = var.region
+  #address = var.lb_ip_addr
 }
