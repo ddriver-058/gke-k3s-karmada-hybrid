102d2a175516b253c8b4108510af39eaca25159b
David Driver <davidallendriver@gmail.com>
2024-05-06 02:11:03 +0000

    add karmada

--- helm/fetchCharts.sh
+++ helm/fetchCharts.sh
@@ -1,2 +1,7 @@
+# Ingress-nginx
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm fetch ingress-nginx/ingress-nginx --version 4.10.1 --untar
+
+# Karmada
+helm repo add karmada-charts https://raw.githubusercontent.com/karmada-io/karmada/master/charts
+helm fetch karmada-charts/karmada --version 1.9.0 --untar
---
+++ karmada/configureKarmada.sh
@@ -0,0 +1,3 @@
+# Install karmada CLI
+# https://karmada.io/docs/next/installation/install-cli-tools
+curl -s https://raw.githubusercontent.com/karmada-io/karmada/master/hack/install-cli.sh | sudo bash
