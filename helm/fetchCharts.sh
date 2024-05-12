f1adf4dae794d19f12255e714194140819c77ca8
David Driver <davidallendriver@gmail.com>
2024-05-12 04:04:57 +0000

    add submariner

--- helm/fetchCharts.sh
+++ helm/fetchCharts.sh
@@ -5,3 +5,7 @@ helm fetch ingress-nginx/ingress-nginx --version 4.10.1 --untar
 # Karmada
 helm repo add karmada-charts https://raw.githubusercontent.com/karmada-io/karmada/master/charts
 helm fetch karmada-charts/karmada --version 1.9.0 --untar
+
+# Submariner
+helm repo add submariner-latest https://submariner-io.github.io/submariner-charts/charts
+helm fetch submariner-latest/submariner-k8s-broker --version 0.17.1 --untar
